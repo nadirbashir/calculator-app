3dac662327792ec8eaa1fe49f6de8de0ef9b965c
nadirbashir <1.nadirbashir@gmail.com>
2021-06-12 00:40:03 +0500

    code update

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 var inp_num;
+var result = document.getElementById("display");
 
 function set(inp_num){
-    var result = document.getElementById("display");
     result.value += inp_num;
 }
-
-
 function clearResult(){
-    var result = document.getElementById("display");
     result.value = "";
 }
 
-
 function getResult(){
-    var result = document.getElementById("display");
     result.value = eval(result.value);
 }
